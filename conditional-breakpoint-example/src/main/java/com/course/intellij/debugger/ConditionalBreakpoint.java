10ae8b65859bf2d310c58084d1d47c7ecc94f885
rejkom <michal.rejkowski@gmail.com>
2025-07-28 15:13:27 +0200

    Refactored initial commit

--- conditional-breakpoint-example/src/main/java/com/course/intellij/debugger/ConditionalBreakpoint.java
+++
@@ -1,9 +0,0 @@
-package com.course.intellij.debugger;
-
-public class ConditionalBreakpoint {
-    public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
-            System.out.println("Current value: " + i);
-        }
-    }
-}
